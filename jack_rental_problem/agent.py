0c63db854875281b8dc7cb9207282a9883e6b9fd
Hy-LeTuan <113957387+Hy-LeTuan@users.noreply.github.com>
2024-10-02 10:25:09 +0700

    update jack rental problem

--- jack_rental_problem/agent.py
+++ jack_rental_problem/agent.py
@@ -2,10 +2,8 @@ import numpy as np
 
 
 class Agent:
-    def __init__(self, cars1, cars2, cars_max: int, actions: list, starting_policy: list, states: list, rewards: list, theta: float, gamma: float):
+    def __init__(self, cars_max: int, actions: list, starting_policy: list, states: list, rewards: list, theta: float, gamma: float):
         # numbers of cars
-        self.cars1 = cars1
-        self.cars2 = cars2
         self.cars_max = cars_max
 
         # variables for reinforcement learing
@@ -35,7 +33,7 @@ class Agent:
     def get_reward(self, reward_index, number_of_cars: int):
         return self.rewards[reward_index] * number_of_cars
 
-    def p(self, s, action_index: tuple, next_day_rental_request_1: int, next_day_rental_request_2: int) -> tuple:
+    def p(self, s, action_index: tuple, today_rental_request_1: int, today_rental_request_2: int, today_customer_return_1, today_customer_return_2) -> tuple:
         # do not include return because returns are only effective till the next day and is therefore added to the number of cars at each location at the end of the loop
 
         # the probability of getting to the next state is 1 -> return next state
@@ -49,6 +47,7 @@ class Agent:
         cars1 = s[0]
         cars2 = s[1]
 
+        # calculate rewards based on today's number of cars
         number_of_cars_moved = self.actions[action_index]
 
         if cars1 < np.abs(number_of_cars_moved) or cars2 < np.abs(number_of_cars_moved):
@@ -63,13 +62,20 @@ class Agent:
         final_reward = np.float32(0)
 
         # if request > cars -> rent all cars. else, rent 'request' numbers of cars
-        reward1 = self.get_reward(0, min(next_day_rental_request_1, cars1))
+        reward1 = self.get_reward(0, min(today_rental_request_1, cars1))
         # if request > cars -> rent all cars. else, rent 'request' numbers of cars
-        reward2 = self.get_reward(0, min(next_day_rental_request_2, cars2))
+        reward2 = self.get_reward(0, min(today_rental_request_2, cars2))
 
         final_reward += reward1
         final_reward += reward2
         final_reward += cost
+
+        # calculate next state using the number of cars returned
+        cars1 += today_customer_return_1
+        cars2 += today_customer_return_2
+
+        cars1 = min(cars1, self.cars_max)
+        cars2 = min(cars2, self.cars_max)
 
         return final_reward, (cars1 - 1, cars2 - 1)
 
@@ -79,7 +85,19 @@ class Agent:
         """
         return self.policy[s]
 
-    def policy_evaluation_step(self, next_day_rental_request_1, next_day_rental_request_2) -> np.float32:
+    def set_action_to_policy(self, action_index: tuple, s: tuple) -> None:
+        """
+        greedily set the new policy to the best action according to the value function 
+        """
+        self.policy[s] = action_index
+
+    def check_valid_value_function_delta(self, delta) -> bool:
+        if delta < self.theta:
+            return True
+        else:
+            return False
+
+    def policy_evaluation_step(self, today_rental_request_1, today_rental_request_2, today_customer_return_1, today_customer_return_2) -> np.float32:
         delta = np.float32(0)
 
         for i in range(self.state_policy_values.shape[0]):
@@ -90,8 +108,8 @@ class Agent:
                 action_index = self.get_action_from_policy(s)
 
                 # get reward and next state
-                reward, next_state = self.p(
-                    s, action_index=action_index, next_day_rental_request_1=next_day_rental_request_1, next_day_rental_request_2=next_day_rental_request_2)
+                reward, next_state = self.p(s, action_index=action_index, today_rental_request_1=today_rental_request_1, today_rental_request_2=today_rental_request_2,
+                                            today_customer_return_1=today_customer_return_1, today_customer_return_2=today_customer_return_2)
 
                 # update current value function of current policy
                 self.state_policy_values[s] = reward + \
@@ -102,34 +120,7 @@ class Agent:
 
         return delta
 
-    def policy_evaluation(self, next_day_rental_request_1, next_day_rental_request_2) -> None:
-        while True:
-            delta = np.float32(0)
-
-            # loop through each state
-            # for i in range(self.state_policy_values.shape[0]):
-            #     for j in range(self.state_policy_values.shape[1]):
-            # s = (i, j)
-            # v = self.state_policy_values[s]
-
-            # # get action index from policy
-            # action_index = self.get_action_from_policy(s)
-
-            # # get reward and next state
-            # reward, next_state = self.p(s, action_index=action_index, next_day_rental_request_1=next_day_rental_request_1, next_day_rental_request_2=next_day_rental_request_2)
-
-            # # update current value function of current policy
-            # self.state_policy_values[s] = reward + self.gamma * self.state_policy_values[next_state]
-
-            delta = max(delta, self.policy_evaluation_step(
-                next_day_rental_request_1, next_day_rental_request_2))
-
-            if delta < self.theta:
-                break
-
-        return
-
-    def policy_improvement_step(self, next_day_rental_request_1, next_day_rental_request_2) -> bool:
+    def policy_improvement_step(self, today_rental_request_1, today_rental_request_2, today_customer_return_1, today_customer_return_2) -> bool:
         """
         -> returns True if the best policy is found, returns false if the policy is not found
         """
@@ -139,15 +130,15 @@ class Agent:
                 s = (i, j)
                 a = self.get_action_from_policy(s)
 
-                # getting the best action for the current state based on the value function
+                # getting the best action for the current state based on the value function. argmax_a p(r, s' | s, a)
                 best_action_index = np.zeros(2)
                 best_action_value = np.float32(0)
 
                 for x in range(self.actions.shape[0]):
                     for y in range(self.actions.shape[1]):
                         action_index = (x, y)
-                        reward, next_state = self.p(
-                            s=s, action_index=action_index, next_day_rental_request_1=next_day_rental_request_1, next_day_rental_request_2=next_day_rental_request_2)
+                        reward, next_state = self.p(s=s, action_index=action_index, today_rental_request_1=today_rental_request_1, today_rental_request_2=today_rental_request_2,
+                                                    today_customer_return_1=today_customer_return_1, today_customer_return_2=today_customer_return_2)
 
                         current_action_value = reward + \
                             self.state_policy_values[next_state]
@@ -155,6 +146,8 @@ class Agent:
                         if current_action_value > best_action_value:
                             best_action_value = current_action_value
                             best_action_index = action_index
+
+                self.set_action_to_policy(best_action_index, s)
 
                 if a != best_action_index:
                     return False
--- jack_rental_problem/environment.py
+++ jack_rental_problem/environment.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tqdm import tqdm
 from agent import Agent
 
 
@@ -13,6 +14,9 @@ class Enviroment:
 
         # intiailize agent
         self.agent = agent
+
+        # initialize policy history
+        self.policy_history = []
 
     def get_rental_requests(self) -> tuple:
         """
@@ -35,3 +39,62 @@ class Enviroment:
             lam=self.expected_return_lambda_2)
 
         return (customer_return_1, customer_return_2)
+
+    def train(self, number_of_days, max_iterations) -> int:
+        """
+        -> returns the number of iteration needed to find an optimal policy, or the number of max iterations if multiple policies are found
+        """
+        for iteration in tqdm(range(max_iterations), desc="Policy Check", total=max_iterations):
+            # save old policy and initialize benchmark variables
+            benchmark_rental_request_1, benchmark_rental_request_2 = self.get_rental_requests()
+            benchmark_customer_return_1, benchmark_customer_return_2 = self.get_customer_returns()
+
+            self.policy_history.append(self.agent.policy)
+
+            # inner loop to evaluate value function
+            for d in tqdm(range(number_of_days), desc="Policy Evaluation", total=number_of_days):
+                today_rental_request_1, today_rental_request_2 = self.get_rental_requests()
+                today_customer_return_1, today_customer_return_2 = self.get_customer_returns()
+
+                delta = self.agent.policy_evaluation_step(today_rental_request_1=today_rental_request_1, today_rental_request_2=today_rental_request_2,
+                                                          today_customer_return_1=today_customer_return_1, today_customer_return_2=today_customer_return_2)
+
+                if self.agent.check_valid_value_function_delta(delta=delta):
+                    break
+
+            # check if current policy is optimal policy
+            has_optimal_policy = self.agent.policy_improvement_step(today_rental_request_1=benchmark_rental_request_1, today_rental_request_2=benchmark_rental_request_2,
+                                                                    today_customer_return_1=benchmark_customer_return_1, today_customer_return_2=benchmark_customer_return_2)
+
+            if has_optimal_policy:
+                return iteration
+
+        return max_iterations
+
+
+if __name__ == "__main__":
+    # initialize agent constants
+    max_number_of_cars = 3
+    available_actions = np.stack([np.arange(
+        0, max_number_of_cars + 1), np.arange(0, -max_number_of_cars - 1, step=-1)])
+
+    # initialize environment constants
+    expected_request_lambda_1 = 3
+    expected_request_lambda_2 = 4
+    expected_return_lambda_1 = 3
+    expected_return_lambda_2 = 2
+
+    number_of_days = 100
+    max_iterations = 5
+
+    # initialize agent
+    agent = Agent(cars_max=max_number_of_cars, actions=available_actions, starting_policy=np.zeros(
+        (3, 3)), states=np.zeros((3, 3)), rewards=[0, 10, -2], theta=1e-3, gamma=0.9)
+
+    # initialize environment
+    environment = Enviroment(expected_request_lambda_1=expected_request_lambda_1, expected_request_lambda_2=expected_request_lambda_2,
+                             expected_return_lambda_1=expected_return_lambda_1, expected_return_lambda_2=expected_return_lambda_2)
+
+    # start training
+    environment.train(number_of_days=number_of_days,
+                      max_iterations=max_iterations)
